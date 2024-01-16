104a236f10a6440df9a73315dcecb5ef050d41de
johanlundberg92 <johan.lundberg1992@gmail.com>
2024-01-16 21:14:47 +0100

    provider fix

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    openstack = {
+      source = "terraform-provider-openstack/openstack"
+      version = "1.53.0"
+    }
+  }
+}
 provider "openstack" {
   user_name = "johan.lundberg@iver.se"
   tenant_name = "Demo-johan.lundberg"
