39928bda9f7404a10410648b311f9a8378d12d72
johanlundberg92 <johan.lundberg1992@gmail.com>
2024-01-16 21:35:37 +0100

    var2

--- variables.tf
+++ variables.tf
@@ -2,6 +2,10 @@ variable "server_name" {
     description = "Unique and catchy name for your server (because naming standards are boring)"
     type = string
 }
+variable "os_password" {
+    description = "Password for openstack connection"
+    sensitive = true
+}
 variable "network_cidr" {
     description = "CIDR notation of the network that will be created (10.0.0.0/24)"
 }
