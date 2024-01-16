49ea076c1d540cc2e35433021f8e8ceb8fab0a40
johanlundberg92 <johan.lundberg1992@gmail.com>
2024-01-16 21:48:21 +0100

    no pw var

--- main.tf
+++ main.tf
@@ -10,6 +10,6 @@ provider "openstack" {
   user_name = "johan.lundberg@iver.se"
   tenant_name = "Demo-johan.lundberg"
   #password = var.os_password
-  password = env("os_password")
+  #password = env("os_password")
   auth_url = "https://sto-api.elits.com:5000/v3"
 }
