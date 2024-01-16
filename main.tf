83ff60e6fe8bc68e6e91f0afe2a485131b9f7e39
johanlundberg92 <johan.lundberg1992@gmail.com>
2024-01-16 21:44:11 +0100

    env var

--- main.tf
+++ main.tf
@@ -9,6 +9,7 @@ terraform {
 provider "openstack" {
   user_name = "johan.lundberg@iver.se"
   tenant_name = "Demo-johan.lundberg"
-  password = var.os_password
+  #password = var.os_password
+  password = env("os_password")
   auth_url = "https://sto-api.elits.com:5000/v3"
 }
